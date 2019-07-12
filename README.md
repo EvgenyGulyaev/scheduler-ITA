bd4466a689ae5b77ed1494883ed509d0951b20c6
Evgeny Gulyaev <wardercompany@gmail.com>
2019-07-12 12:37:05 +0300

    use Axios

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # scheduler-ITA
-It is repo for  scheduler-ITA parser
+Данная библиотека предназначена для работы с `http://ictis.sfedu.ru/rasp/`
+
+## Установка 
+Используя npm:    
+`npm install scheduler-ITA --save`
+
+
+Используя yarn:     
+`yarn add scheduler-ITA`
+
+## Об типах страниц на данном сайт
+
+`http://ictis.sfedu.ru/rasp/HTML_AUDS/a4.htm` - HTML_AUDS означает, что расписание для аудитории  
+`http://ictis.sfedu.ru/rasp/HTML/18.htm`  - HTML означает, что расписание для групп 
+`http://ictis.sfedu.ru/rasp/HTML_PREPS/m18.htm` - HTML_PREPS означает, что расписание для преподавателей 
+
+
+## Методы 
+
+`getData(number, type = HTML)`    
+Получение данных по заданному номеру с типом HTML по умолчанию, но можно использовать и другие типы как например 
+HTML_PREPS и HTML_AUDS, под номером подразумевается число, например 18 в данном url `http://ictis.sfedu.ru/rasp/HTML/18.htm` 
+
+
+`getName(number, type = HTML)`
+Получение имени группы или объекта по заданному номеру с типом HTML по умолчанию, но можно использовать и другие типы как например 
+HTML_PREPS и HTML_AUDS, под номером подразумевается число, например 18 в данном url `http://ictis.sfedu.ru/rasp/HTML/18.htm` 
+
+
+`getDataForRoom(number, type = HTML_AUDS)`
+Аналог метода getData, но по умолчанию тип запроса HTML_AUDS
+
+
+`getDataForRoom(number, type = HTML_PREPS)`
+Аналог метода getData, но по умолчанию тип запроса HTML_PREPS
+
+## Примеры
+
+```
+const { getData } = require('scheduler-ita');
+
+(async () => {
+  const data = await getData(18);
+  console.log('data',  data );
+})();
+
+
+```
+
--- index.js
+++ index.js
@@ -1,81 +1,64 @@
-const request = require('request'),
-      cheerio = require('cheerio'),
-      url = 'http://ictis.sfedu.ru/rasp/HTML/18.htm';
+const
+  cheerio = require('cheerio'),
+  url = 'http://ictis.sfedu.ru/rasp/',
+  html = 'HTML',
+  htmlAuds = 'HTML_AUDS',
+  htmlPreps = 'HTML_PREPS',
+  _get = require('lodash.get'),
+  axios = require('axios'),
+  _chunk = require('lodash.chunk');
 
-const quantityPinDay = 5;
-const quantityPinCell = 3;
+const getContent = (p) => {
+  const schedule = p.map(el => _get(el, 'children[0].children[0].data', ''));
+  return _chunk(schedule, 65)
+}
 
-const getElement = (element) => {
-  if (Array.isArray(element)){
-    if(element.length === quantityPinDay || element.length === quantityPinCell){
-      let flag = true;
-      let childrenData = element[element.length-1];
-      while( flag ){
-        let { children } = childrenData;
-        if (!children || !children.length) {
-          let { data } = childrenData;
-          flag = false;
-          return data;
-        } else {
-          childrenData = children[0];
-        }
-      }
+const getNameId = ($, body) => {
+  const startWeekAndName = $("p", body).first().text().split(':');
+  const firstWeek = startWeekAndName[2].split('-')[0];
+  const name = startWeekAndName[1].split(' ')[0];
+  return { name, firstWeek }
+}
 
-    }
-  } else {
-    return {};
-  }
-};
-
-
-request(url, function (error, response, body) {
-  const status = 'Ok';
-  if (!error) {
-    const  $ = cheerio.load(body);
-    const startWeekAndName = $("p", body).first().text().split(':');
-    const firstWeek = startWeekAndName[2].split('-')[0];
-    const name = startWeekAndName[1].split(' ')[0];
-    const tables = $("table", body).toArray();
-    const schedule = tables.map( table => {
-      let prettiedTable = table.children[1].children;
-      const elData = prettiedTable.map( tr => {
-        const { type } = tr;
-        if(type === 'tag'){
-          const trData = tr.children.map( td => {
-            const { children: tdInfo = {}, type: curType = 'tag' } = td;
-            if(curType === 'tag'){
-              const data =   getElement(tdInfo);
-              console.log("Abra", data);
-            }
-          });
-        }
-      });
-      console.log("Abra",  table);
-    });
-
-
-    return {name, firstWeek, status}
-  } else {
-    return {status: 'Bad'}
-  }
-});
-
-
-exports.getData = function(number) {
-
-};
-
-
-exports.getName = function(number) {
-  request(`${url}${number}.htm`, function (error, response, body) {
+const getDataForType = async (number, type = html) => {
+  try {
+    const { data } = await axios.get(`${url}${type}/${number}.htm`);
     const status = 'Ok';
-    if (!error) {
-      const  $ = cheerio.load(body);
-      const startWeekAndName = $("p", body).first().text().split(':');
-      const name = startWeekAndName[1].split(' ')[0];
-      return {name, status}
-    } else {
-      return {name: '', status: 'Bad'}
-    }
-  });
+    const $ = cheerio.load(data);
+    const nameData = getNameId($, data);
+    const p = $("p", data).toArray();
+    const schedule = getContent(p);
+    return { ...nameData, status, schedule }
+  }
+  catch (e) {
+    console.log('error', e);
+    return { status: 'Bad' }
+  }
+}
+
+const getData = (number, type = html) => getDataForType(number, type);
+
+const getDataForRoom = (number, type = htmlAuds) => getDataForType(number, type);
+
+const getDataForTeacher = (number, type = htmlPreps) => getDataForType(number, type);
+
+const getName = async (number, type = html) => {
+  try {
+    const { data } = await axios.get(`${url}${type}/${number}.htm`);
+    const status = 'Ok';
+    const $ = cheerio.load(data);
+    const { name } = getNameId($, data);
+    return { name, status }
+  }
+  catch (error) {
+    console.log('error', error);
+    return { name: '', status: 'Bad' }
+  }
 };
+
+module.exports = {
+  getName,
+  getDataForTeacher,
+  getData,
+  getDataForRoom,
+}
